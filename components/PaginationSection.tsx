a2a17c51fdbe22a716adc82996aa4c6733feb589
Bartek Z <bartekzarzyk@gmail.com>
2024-02-04 14:01:30 +0100

    remove h-full class - incorrect display height on mobile

--- components/PaginationSection.tsx
+++ components/PaginationSection.tsx
@@ -25,7 +25,7 @@ const PaginationSection = ({
 
   return (
     <>
-      <section className='mt-6 grid h-full grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+      <section className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
         {currentPosts.map((product, idx: number) => (
           <CardClient
             key={product._id}
